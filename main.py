5726cf163b5320c7115b9d54a44fd45b19cc06f9
UnsignedArduino <UnsignedArduino@outlook.com>
2021-10-17 19:46:12 -0400

    Can write only parsed output to standard output

--- main.py
+++ main.py
@@ -8,14 +8,23 @@ parser.add_argument("path", type=Path,
                     help="The path to the MIDI file")
 parser.add_argument("--output_path", type=Path,
                     default=None, help="The path to write the images to")
+parser.add_argument("--stdout", action="store_const",
+                    const=True, default=False,
+                    help="Whether to output everything to standard output "
+                         "instead of writing to a file. No info messages "
+                         "will be written. ")
 args = parser.parse_args()
+
 in_path = args.path.expanduser().resolve()
 out_path = args.output_path
+to_stdout = args.stdout
 if out_path is None:
     out_path = in_path.parent / (in_path.stem + ".txt")
 
-print(f"Path to MIDI file is {in_path}")
+if not to_stdout:
+    print(f"Path to MIDI file is {in_path}")
 
+# Parse here
 
-
-print(f"Output destination file is {out_path}")
+if not to_stdout:
+    print(f"Output destination file is {out_path}")
